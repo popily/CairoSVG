b3cb3cbade777b3a9c62f0251af52ec801097107
Jonathon Morgan <jonathon@goodattheinternet.com>
2015-11-30 01:06:51 -0600

    updating pngsurface for retina display

--- cairosvg/surface.py
+++ cairosvg/surface.py
@@ -448,7 +448,7 @@ class PSSurface(Surface):
 
 class PNGSurface(Surface):
     """A surface that writes in PNG format."""
-    device_units_per_user_units = 1
+    device_units_per_user_units = 2
 
     def _create_surface(self, width, height):
         """Create and return ``(cairo_surface, width, height)``."""
